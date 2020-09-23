8c99493b6d27fa63ce54ac4ac8e36a891252ba5f
AdamArzaqi <arzaqiadam45@gmail.com>
2020-09-23 16:30:43 +0700

    Tes Klik Button

--- app/src/androidTest/java/com/example/new_sample/TesKlikButton.java
+++ app/src/androidTest/java/com/example/new_sample/TesKlikButton.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.longClick;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -21,10 +22,86 @@ public class TesKlikButton {
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void tesKlikButtonSatu(){
-        //cari id button1 terus di klik
+    public void click_number_btn() {
         onView(withId(R.id.button1)).perform(click());
-        onView(withId(R.id.input)).check(matches(withText("1")));
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.button3)).perform(click());
+        onView(withId(R.id.button4)).perform(click());
+        onView(withId(R.id.button5)).perform(click());
+        onView(withId(R.id.button6)).perform(click());
+        onView(withId(R.id.button7)).perform(click());
+        onView(withId(R.id.button8)).perform(click());
+        onView(withId(R.id.button9)).perform(click());
+        onView(withId(R.id.button0)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("1234567890")));
+    }
+
+    @Test
+    public void force_operational_btn(){
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("Error")));
+        onView(withId(R.id.button_clear)).perform(longClick());
+
+        onView(withId(R.id.button_sub)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("Error")));
+        onView(withId(R.id.button_clear)).perform(longClick());
+
+        onView(withId(R.id.button_divide)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("Error")));
+        onView(withId(R.id.button_clear)).perform(longClick());
+
+        onView(withId(R.id.button_multi)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("Error")));
+        onView(withId(R.id.button_clear)).perform(longClick());
+
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("Error")));
+        onView(withId(R.id.button_clear)).perform(longClick());
+    }
+
+    @Test
+    public void tambah_baru(){
+        onView(withId(R.id.button1)).perform(click());
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.button1)).perform(click());
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("2")));
+    }
+
+    @Test
+    public void kurang_baru(){
+        onView(withId(R.id.button1)).perform(click());
+        onView(withId(R.id.button_sub)).perform(click());
+        onView(withId(R.id.button1)).perform(click());
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("0")));
+    }
+
+    @Test
+    public void kali_baru(){
+        onView(withId(R.id.button5)).perform(click());
+        onView(withId(R.id.button_multi)).perform(click());
+        onView(withId(R.id.button5)).perform(click());
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("25")));
+    }
+
+    @Test
+    public void bagi_baru(){
+        onView(withId(R.id.button5)).perform(click());
+        onView(withId(R.id.button_divide)).perform(click());
+        onView(withId(R.id.button5)).perform(click());
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("1")));
+    }
+
+    @Test
+    public void modulus_simple(){
+        onView(withId(R.id.button5)).perform(click());
+        onView(withId(R.id.button_para1)).perform(click());
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.output)).check(matches(withText("1")));
     }
 
 }
